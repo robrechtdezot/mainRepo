44f47af5d3b71b667c272fa7126ee91982e94cf1
wout- <woutservaes1@gmail.com>
2025-03-04 00:31:10 +0100

    rpg 03.4

---
+++ rpg/SOSCRPG/Engine/ViewModels/Gamesesion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;    
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Wotto";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
