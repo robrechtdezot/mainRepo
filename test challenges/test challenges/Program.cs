cbb4c075372a30b88840a69db9a41629a9a9daf9
wout- <woutservaes1@gmail.com>
2025-03-04 00:05:26 +0100

    de rest

--- test challenges/test challenges/Program.cs
+++ test challenges/test challenges/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,25 +13,14 @@ namespace test_challenges
     {
         static void Main(string[] args)
         {
-            string text = "He World";
-            string text2 = text.ToLower();
-            var groupedCharacters = text2.GroupBy(c => c)
-                                         .Select(g => new { Character = g.Key, Count = g.Count() })
-                                         .OrderByDescending(g => g.Count)
-                                         .ToList();
-            foreach (var item in groupedCharacters)
-            {
-                if ( item.Count > 1)
-                {
-                    Console.WriteLine(groupedCharacters.First());
-                    break;
-                }
-                else //if (item.Count <= 1)
-                {
-                    Console.WriteLine("No repeated characters");
-                    break;
-                }
-            }//hallo
+            IntroduceFriends();
+            }
+
+
+        public static void IntroduceFriends(string name1, string name2, string name3)
+        {
+            Console.WriteLine($"These are my friends, {name1} and {name2}!");
+            Console.WriteLine($"\"{name1}\", \"{name2}\",\"{name3}\"");
         }
     }
 }
